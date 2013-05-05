7f55bf571b29bee58d91af5051d324bb4cb78369
Apprentice <apprentice@dbc02.local>
2013-05-05 13:52:15 -0700

    comment out sleep timer in index controller

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -8,7 +8,7 @@ post '/rolls' do
   content_type :json
   # If the user passes-in a "value", let's use it. Otherwise, we'll generate a random one.
   # See: roll_if_value_is_nil method in the Roll model.
-  sleep 2
+  # sleep 2
   value = params[:value] ? params[:value].to_i : nil
   roll = value ? Roll.create({ value: value }) : Roll.create
   {:roll => roll.value }.to_json
